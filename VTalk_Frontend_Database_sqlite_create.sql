21b278c7648bbfe01db210988ee2f44db55c2bfb
vishu160196 <vishu160196@gmail.com>
2017-10-09 12:31:18 +0530

    Create Tables

---
+++ VTalk_Frontend_Database_sqlite_create.sql
@@ -0,0 +1,17 @@
+CREATE TABLE message_info (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	content text,
+	state BOOLEAN NOT NULL,
+	sender_id integer NOT NULL,
+	receiver_id integer NOT NULL,
+	FOREIGN KEY(sender_id) REFERENCES contact(contact_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY(receiver_id) REFERENCES contact(contact_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE contact (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	contact_id integer UNIQUE,
+	name text,
+	username text
+);
+
---
+++ app/src/main/java/com/example/vishal/vtalk/Messages.java
@@ -0,0 +1,24 @@
+package com.example.vishal.vtalk;
+
+/**
+ * Class for Messages table
+ */
+
+import android.provider.BaseColumns;
+
+public final class Messages {
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private Messages() {}
+
+    /* Inner class that defines the table contents */
+    public static class FeedEntry implements BaseColumns {
+        public static final String TABLE_NAME = "message_info";
+        public static final String id = "id";
+
+        public static final String content = "content";
+        public static final String state = "state";
+        public static final String sender_id = "sender_id";
+        public static final String receiver_id = "receiver_id";
+    }
+}
